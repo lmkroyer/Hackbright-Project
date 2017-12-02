7a97366593536b041aad686fca99378b9869f6e5
lmariek <lmkroyer@gmail.com>
2017-12-02 11:21:16 -0800

    tweaks to styling

--- static/homepage.js
+++ static/homepage.js
@@ -8,9 +8,84 @@ $('#home-img').hover(function(){
 }, function(){
     $('#darkness').fadeTo(100, 0, function(){
         $(this).show();
-        $('#login-route').show();
+        // $('#login-route').show();
     });
 });
+
+// ;(function($) {
+
+// $.fn.letterDrop = function() {
+//   // Chainability
+//   return this.each( function() {
+
+//   let obj = $( this );
+
+//   let drop = {
+//     arr : obj.text().split( '' ),
+
+//     range : {
+//       min : 1,
+//       max : 9
+//     },
+
+//     styles : function() {
+//       let dropDelays = '\n', addCSS;
+
+//        for ( i = this.range.min; i <= this.range.max; i++ ) {
+//          dropDelays += '.ld' + i + ' { animation-delay: 1.' + i + 's; }\n';
+//        }
+
+//         addCSS = $( '<style>' + dropDelays + '</style>' );
+//         $( 'head' ).append( addCSS );
+//     },
+
+//     main : function() {
+//       let dp = 0;
+//       obj.text( '' );
+
+//       $.each( this.arr, function( index, value ) {
+
+//         dp = dp.randomInt( drop.range.min, drop.range.max );
+
+//         if ( value === ' ' )
+//           value = '&nbsp';
+
+//           obj.append( '<span class="letterDrop ld' + dp + '">' + value + '</span>' );
+
+//       });
+
+//     }
+//   };
+
+//   Number.prototype.randomInt = function ( min, max ) {
+//     return Math.floor( Math.random() * ( max - min + 1 ) + min );
+//   };
+
+
+//   // Create styles
+//   drop.styles();
+
+
+//     // Initialise
+//     drop.main();
+//   });
+
+// };
+
+// }(jQuery));
+
+
+// USAGE
+$('#login-route').hide();
+
+$('#home-img').hover(function(){
+    $('#login-route').show();
+    $( '#login-route' ).letterDrop();
+});
+
+// $( '#greet-user' ).letterDrop();
+
+
 
 // $('#home-img').hover(function(){
 //     $('#home-img').fadeTo(200, 0.1);
