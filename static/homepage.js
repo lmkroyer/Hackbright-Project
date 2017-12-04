920e25466a8adc18fc72dc02b274df2ece64447c
lmariek <lmkroyer@gmail.com>
2017-12-04 14:47:51 -0800

    login hover works

--- static/homepage.js
+++ static/homepage.js
@@ -3,19 +3,42 @@
 // HOMEPAGE FADE IMAGE //
 /////////////////////////
 
-$('#home-img').hover(function(){
-    $('#darkness').fadeTo(25, 1);
-}, function(){
-    $('#darkness').fadeTo(25, 0, function(){
-        $(this).show();
-        $('#login-route').hide();
-    });
-});
-
-// USAGE
 $('#login-route').hide();
 
-$('#home-img').hover(function(){
-    $('#login-route').show();
-});
+$('#home-img, .gallery-text, #login-route').hover(
+    function() {
+        $('#darkness').fadeTo(25, 1);
+        $("#home-img").css("-webkit-filter", "brightness(200%)");
+        $('#login-route').show();
+    },
+    function(evt){
+        if (evt.toElement === $('#darkness')[0]) {
+            $('#darkness').fadeTo(25, 0);
+            $("#home-img").css("-webkit-filter", "brightness(100%)");
+            $('#login-route').hide();
+        }
+    }
+);
+
+// $('#home-img, .gallery-text, #login-route').hover(
+//     function() {
+//         $('#darkness').fadeTo(25, 1);
+//         $("#home-img").css("-webkit-filter", "brightness(200%)");
+//     },
+//     function(){
+//         $('#darkness').fadeTo(25, 0,
+//             function(){
+//                 $(this).show();
+//             }
+//         );
+//         $("#home-img").css("-webkit-filter", "brightness(100%)");
+//     }
+// );
+
+// USAGE
+
+
+// $('#home-img').hover(function(){
+//     $('#login-route').show();
+// });
 
